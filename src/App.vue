a479f643b2f26d042781d1524f7633c8be06b734
Mogakamo <mogaka.amo254@gmail.com>
2021-09-03 09:41:05 +0300

    Added clipboardjs, bootstrap for styling and configured it

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
 
 <script>
 import Home from './components/Home.vue'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 export default {
   name: 'App',
